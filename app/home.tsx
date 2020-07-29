639fefe8478f272ca4adb9f9af48d1238340ec0b
lazaryan <lazaryan99@gmail.com>
2020-07-30 01:07:50 +0300

    add routings

---
+++ app/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { View } from 'react-native'
+import { Button } from '@ui'
+
+export const Component = () => {
+    return (
+        <View>
+            <Button title="Поиск" onPress={() => {}} />
+        </View>
+    )
+}
+
+export default Component
--- app/index.tsx
+++ app/index.tsx
@@ -5,20 +5,43 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
+import 'react-native-gesture-handler'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import { ThemeProvider } from 'styled-components/native'
-import { Button } from '@ui';
 import theme from '@theme'
+
+import Home from './home'
+
+const Stack = createStackNavigator();
 
 const App: React.SFC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Button title="Поиск" onPress={() => {}} />
-    </ThemeProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            textAlign: 'center'
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerStyle: {
+              height: 0
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 
-export default App;
+export default () => <ThemeProvider theme={theme}><App /></ThemeProvider>;
